8a9cbc8932ad96560a30207188eeef97056ea044
cj-Pacific-Blue-IT <57339692+cj-Pacific-Blue-IT@users.noreply.github.com>
2020-06-15 21:51:46 +0800

    Login & Logout

--- 3AB/public/controllers/DashboardCtrl.js
+++ 3AB/public/controllers/DashboardCtrl.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('newApp').controller('DashboardCtrl', function($scope) {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //     if (user) {
 
-            console.log(user);
+    //         console.log(user);
 
-        } else {
-            console.log(window.location.pathname);
-            // window.location.pathname = '/3AB/public/login.html';
-            window.location.href = './index.html';
-        }
-    });
+    //     } else {
+    //         console.log(window.location.pathname);
+    //         // window.location.pathname = '/3AB/public/login.html';
+    //         window.location.href = './index.html';
+    //     }
+    // });
 });
