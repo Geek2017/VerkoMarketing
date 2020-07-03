fc02413066ccdbb23a8f2d4adba49f7e38f40304
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2020-07-03 22:19:18 +0800

    123

--- Structure/ReadMore/assets/js/call.js
+++ Structure/ReadMore/assets/js/call.js
@@ -1,34 +1,77 @@
-$(document).ready(function() {
-    new jmbotdetector({
-        timeout: 15000,
-        callback: function(result) {
+new jmbotdetector({
+    timeout: 5000,
+    callback: function(result) {
 
-            console.log('result:', result.tests)
+        console.log('result:', result.tests)
 
-            $.getJSON("../js/data.json", function(data) {
-                console.log(data.bot);
-                console.log(data.human);
+        $.getJSON("../js/data.json", function(data) {
+            console.log(data.bot);
+            console.log(data.human);
 
-                var bot = data.bot;
-                var human = data.human;
+            var human = data.human;
+            var bot = data.bot;
 
-                console.log(bot);
+            if (result.cases.mousemove) {
 
-                if (result.cases.mousemove) {
+                var currentUrl = window.location.hostname;
+                console.log(currentUrl);
+
+                var today = new Date();
+                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+                var datetime = today + '/' + time;
+
+                var uid = firebase.database().ref().child('users').push().key;
+
+                var data = {
+                    'datetime': datetime,
+                    'events': result.cases,
+                    'url': currentUrl
+
+                };
+
+                var updates = {};
+                updates['/datasets/human/' + uid] = data;
+                firebase.database().ref().update(updates, console.log('Human Events Click Save!'));
+                setTimeout(function() {
+
                     console.log('MOUSEMOVE', result.cases.mousemove)
 
                     localStorage.setItem('adsurl', human)
 
                     $(window).attr('location', human)
+                }, 3000);
 
-                } else {
+            } else {
+                var currentUrl = window.location.hostname;
+                console.log(currentUrl);
+
+                var today = new Date();
+                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+                var datetime = today + '/' + time;
+
+                var uid = firebase.database().ref().child('users').push().key;
+
+                var data = {
+                    'datetime': datetime,
+                    'events': result.cases,
+                    'url': currentUrl
+
+                };
+
+                var updates = {};
+                updates['/datasets/bot/' + uid] = data;
+                firebase.database().ref().update(updates, console.log('Bot Events Click Save!'));
+                setTimeout(function() {
 
                     localStorage.setItem('adsurl', bot)
 
                     $(window).attr('location', bot)
-                }
-            })
 
-        }
-    }).monitor();
-});
+                }, 300);
+            }
+        })
+
+    }
+}).monitor();
